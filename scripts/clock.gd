4fe31bbf1c5e4b184f10ae1cafe2026740f16f9e
David Vezzani <dcvezzani@churchofjesuschrist.org>
2020-01-01 14:43:16 -0700

    adds score keeping

--- scripts/clock.gd
+++ scripts/clock.gd
@@ -4,6 +4,8 @@ extends Area2D
 # var a = 2
 # var b = "text"
 
+var time = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,9 +17,10 @@ func _ready():
 
 func _on_clock_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT:
+		if event.button_index == BUTTON_LEFT && event.pressed:
 			print("mouse button click detected")
-			self.queue_free()
+			get_parent().get_parent().kill_clock(self)
 
 func set_time(time):
+	self.time = time
 	$time.text = str(time)
--- scripts/level-gameplay.gd
+++ scripts/level-gameplay.gd
@@ -6,10 +6,16 @@ extends Node2D
 
 var current_time = null
 var available_times = [1,2,3,4,5,6,7,8,9,10]
+var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var score = get_tree().get_nodes_in_group("score")[0]
+	score.text = str(self.score)
+	
+	var clock_value = get_tree().get_nodes_in_group("clock-value")[0]
 	current_time = available_times[0]
+	clock_value.text = str(current_time)
 	create_clocks()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,3 +32,12 @@ func create_clocks():
 		clock.set_time(available_times.pop_front())
 	self.add_child(clocks)
 	
+func kill_clock(clock):
+	if clock.time == current_time:
+		self.score += 10
+		clock.queue_free()
+	else:
+		self.score -= 5
+		
+	var score = get_tree().get_nodes_in_group("score")[0]
+	score.text = str(self.score)
