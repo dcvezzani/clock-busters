d692bc05d974521092a12d7be521f8bb9609b96b
Jalen Morgan <jalen.morgan@byu.edu>
2020-02-01 09:17:19 -0700

    update code

--- scenes/clock_formation.gd
+++ scenes/clock_formation.gd
@@ -5,13 +5,20 @@ extends Node2D
 # var b = "text"
 export var withdrawSpeed = 10
 var isWithdrawing = false
+var withdrawAmount = 500
+var clockController = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func withdraw():
+func withdraw(objIn):
 	isWithdrawing = true
+	clockController = objIn
 	
 func _process(delta):
-	position.y-= withdrawSpeed
+	if isWithdrawing == true:
+		position.y-= withdrawSpeed
+		if position.y < - withdrawAmount:
+			clockController.on_clocks_withdrawn()
+		
